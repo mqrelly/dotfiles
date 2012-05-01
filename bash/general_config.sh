9e6e7e42cf5dff196c45f855b1345eb8584a3122
Szabadkia Márk <mqrelly@gmail.com>
2012-05-01 10:28:50 +0200

    Add basic bash configs.

---
+++ bash/general_config.sh
@@ -0,0 +1,8 @@
+# Useful aliases
+alias g=git
+
+# Prevent prompt displacement after hitting ESC on previous command.
+PS1="\[\033[G\]$PS1"
+
+# Load auto-completion files
+source .bash/completion/*

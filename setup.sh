270ad98a9f95a08be939c04769e02df649da587d
Szabadkia Márk <mqrelly@gmail.com>
2015-03-28 18:09:25 +0100

    [RUBY] Set --no-document the default for gem install.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ SETUP_FILE=`pwd`/$0
 DOTFILES_DIR=$(dirname "$SETUP_FILE")
 
 pushd ~
+
 ln --symbolic --force "$DOTFILES_DIR/hg/hgrc" .hgrc
 ln --symbolic --force "$DOTFILES_DIR/git/gitconfig" .gitconfig
 ln --symbolic --force "$DOTFILES_DIR/vim/vimrc" .vimrc
@@ -12,6 +13,7 @@ ln --symbolic "$DOTFILES_DIR/vim/" .vim
 ln --symbolic --force "$DOTFILES_DIR/tmux/tmux.conf" .tmux.conf
 rm --force .bash
 ln --symbolic --force "$DOTFILES_DIR/bash/" .bash
+ln --symbolic --force "$DOTFILES_DIR/gemrc" .gemrc
 echo "source \"$HOME/.bash/general_config.sh\"" >> .bashrc
 
 popd
